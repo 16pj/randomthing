b642498d6d412123bd729acf8e6bbbdc1a08ac7e
robin <you@example.com>
2017-10-11 15:14:22 +0200

    square function tested2

--- test.py
+++ test.py
@@ -6,21 +6,13 @@ def test_print():
 	out, err = proc.communicate()
 	assert out == "hello_world"
 
-
-def test_wrong_print():
-        cmd = 'curl localhost:5000'
-        proc = Popen(cmd, shell=True, stdout=PIPE)
-        out, err = proc.communicate()
-        assert out != "hello_world":
-
-
 def test_square():
 	test_val = 2
         cmd = 'curl localhost:5000/square/' + str(test_val)
         proc = Popen(cmd, shell=True, stdout=PIPE)
         out, err = proc.communicate()
-        assert out == 4:
+        assert out == 4
+
 
 test_print()
-test_wrong_print()
 test_square()
